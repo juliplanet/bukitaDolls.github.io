ba3f2d689de7787970333a7718be67be5dc7eb4f
juliplanet <ledcreed7@gmail.com>
2020-11-11 19:43:06 +0100

    componentesBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+
+const routes:Routes = [
+  {path:'' , component: InicioComponent},
+  {path:'contacto' , component: ContactoComponent},
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InicioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
